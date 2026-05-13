957cd2d3b46f730b441089251b876e01db2f79d1
Sysix <sysix@sysix-coding.de>
2026-05-13 22:17:44 +0200

    fix: load server envs from interactive shell env

--- client/tools/lsp_helper.ts
+++ client/tools/lsp_helper.ts
@@ -2,16 +2,95 @@ import * as path from "node:path";
 import { LogOutputChannel, window } from "vscode";
 import { Executable, MessageType, ShowMessageParams } from "vscode-languageclient/node";
 import type { BinarySearchResult } from "../findBinary";
+import { execFile } from "node:child_process";
+import { promisify } from "node:util";
 
-export function runExecutable(
+const execFileAsync = promisify(execFile);
+
+let cachedEnv: Record<string, string> | undefined;
+
+/***
+ * Get the shell environment variables by running a login shell and executing `env -0`.
+ * This is necessary because the VSCode extension host process may not have the same environment variables as the user's shell,
+ * which can lead to issues when running the language server that rely on certain environment variables.
+ * e.g., PATH for starting the wrapper npm/pnpm node script like ` exec node  "$basedir/../oxlint/bin/oxlint" "$@"`.
+ *
+ * Mac/Linux GUI shells (which does happen in VS Code context, because of Electron) do not load `.bashrc` or `.zshrc` for non-interactive shells,
+ * and thus the language server fails to start with "command not found: node".
+ */
+export async function getShellEnv(): Promise<Record<string, string>> {
+  if (cachedEnv) {
+    return cachedEnv;
+  }
+
+  const shell = process.env.SHELL ?? "/bin/bash";
+
+  // POSIX shells
+  if (process.platform == "win32") {
+    // windows electron app does not have the problem of individual shell environment, as it inherits the environment from the parent process,
+    cachedEnv = { ...process.env } as Record<string, string>;
+    return cachedEnv;
+  }
+  // Run the shell as a login shell to get the environment variables. The `-i` flag is for interactive shell, which is needed to load the shell configuration files.
+  // The `-l` flag is for login shell, which is needed to load the environment variables defined in the shell configuration files.
+  const { stdout } = await execFileAsync(shell, ["-ilc", "env -0"], {
+    env: {
+      HOME: process.env.HOME,
+    },
+  });
+
+  cachedEnv = {};
+
+  for (const entry of stdout.split("\0")) {
+    if (!entry) continue;
+
+    const i = entry.indexOf("=");
+
+    if (i === -1) continue;
+
+    cachedEnv[entry.slice(0, i)] = entry.slice(i + 1);
+  }
+
+  return cachedEnv;
+}
+
+export async function runExecutable(
   binary: BinarySearchResult,
   useExecPath: boolean = false,
   nodePath?: string,
   tsgolintPath?: string,
   suppressProgramErrors?: boolean,
-): Executable {
+): Promise<Executable> {
+  const processEnv = process.env;
+  const shellEnv = await getShellEnv();
+
+  // only for debugging purpose
+  {
+    if (Object.keys(processEnv).length !== Object.keys(shellEnv).length) {
+      const missingInProcessEnv = Object.keys(shellEnv).filter((key) => !(key in processEnv));
+      const missingInShellEnv = Object.keys(processEnv).filter((key) => !(key in shellEnv));
+
+      console.warn("Process env and shell env have different number of keys", {
+        missingInProcessEnv,
+        missingInShellEnv,
+      });
+    }
+
+    if (processEnv.PATH !== shellEnv.PATH) {
+      const processEnvPATH = processEnv.PATH?.split(path.delimiter) ?? [];
+      const shellEnvPATH = shellEnv.PATH?.split(path.delimiter) ?? [];
+      const missingInProcessEnvPATH = shellEnvPATH.filter((p) => !processEnvPATH.includes(p));
+      const missingInShellEnvPATH = processEnvPATH.filter((p) => !shellEnvPATH.includes(p));
+
+      console.warn("Process env and shell env have different PATH values", {
+        missingInProcessEnvPATH,
+        missingInShellEnvPATH,
+      });
+    }
+  }
+
   const serverEnv: Record<string, string> = {
-    ...process.env,
+    ...shellEnv,
     RUST_LOG: process.env.RUST_LOG || "info", // Keep for backward compatibility for a while
     OXC_LOG: process.env.OXC_LOG || "info",
     NO_COLOR: "1",
