efd4d97b763005e9ebd55721c00e5aff206e60cb
Alexander S. <sysix@sysix-coding.de>
2026-07-24 16:00:21 +0200

    chore: update dependencies (#337)
    * chore: update dependencies

--- tests/integration/code_actions.spec.ts
+++ tests/integration/code_actions.spec.ts
@@ -49,7 +49,7 @@ suite("code actions linter", () => {
     await loadFixture("debugger_empty_config");
     await sleep(500);
     const fileUri = Uri.joinPath(fixturesWorkspaceUri(), "fixtures", "debugger.js");
-    // await window.showTextDocument(fileUri); -- should also work without opening the file
+    await window.showTextDocument(fileUri);
 
     const codeActions: ProviderResult<Array<CodeAction>> = await commands.executeCommand(
       "vscode.executeCodeActionProvider",
